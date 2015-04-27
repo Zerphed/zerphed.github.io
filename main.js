c582dd4f9bdb4013eb2b71d16923d4e5481a080f
Joonas Nissinen <joonas.nissinen@aalto.fi>
2015-04-27 16:56:59 +0300

    Logging subscription information

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function sendSubscriptionToServer(subscription) {
   // TODO: Send the subscription.subscriptionId and
   // subscription.endpoint to your server and save
   // it to send a push message at a later date
+  console.log(subscription);
   console.log('TODO: Implement sendSubscriptionToServer()');
 }
 
