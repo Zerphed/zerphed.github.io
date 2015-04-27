043093312c0a872df5ff7d4aea0f3f979988aa2a
Joonas Nissinen <joonas.nissinen@aalto.fi>
2015-04-27 16:39:54 +0300

    Fixed indent and a couple of typos

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,73 @@
-'use strict';
-
 self.addEventListener('push', function(event) {
-  event.waitUntil(
-    console.log('Received a push message', event);
+    'use strict';
 
-    fetch("https://patient-ui.firebaseio.com/rest/push.json").then(function(response) {
+    event.waitUntil(
+        console.log('Received a push message', event);
 
-      if (response.status !=== 200) {
-        console.error('Looks like there was a problem with fetch. Status code: ' + response.status);
-        throw new Error();
-      }
+        fetch("https://patient-ui.firebaseio.com/rest/push.json").then(function(response) {
 
-      return response.json().then(function(data) {
-        if (data.error) {
-          console.error('The API returned an error', data.error);
-          throw new Error();
-        }
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem with fetch. Status code: ' + response.status);
+                throw new Error();
+            }
 
-        console.log("Received push message information from Firebase.");
+            return response.json().then(function(data) {
+                if (data.error) {
+                    console.error('The API returned an error', data.error);
+                    throw new Error();
+                }
 
-        var title = data.title;
-        var body = data.body;
-        var icon = '/img/icon-192x192.png';
-        var tag = 'simple-push-demo-notification-tag';
+                console.log('Received push message information from Firebase.');
 
-        return self.registration.showNotification(title, {
-          body: body,
-          icon: icon,
-          tag: tag
-        });
-      });
-  }).catch(function(err) {
-    console.log('Unable to retrieve data from Firebase', err)
+                var title = data.title;
+                var body = data.body;
+                var icon = '/img/icon-192x192.png';
+                var tag = 'simple-push-demo-notification-tag';
 
-    var title = 'An error occurred';
-    var body = 'We were unable to get information for this push message.';
-    var icon = '/img/icon-192x192.png';
-    var tag = 'simple-push-demo-notification-tag';
+                return self.registration.showNotification(title, {
+                    body: body,
+                    icon: icon,
+                    tag: tag
+                });
+            });
+        }).catch(function(err) {
+            console.log('Unable to retrieve data from Firebase', err)
 
-    return self.registration.showNotification(title, {
-        body: body,
-        icon: icon,
-        tag: tag
-    });
-    })
-  );
+            var title = 'An error occurred';
+            var body = 'We were unable to get information for this push message.';
+            var icon = '/img/icon-192x192.png';
+            var tag = 'simple-push-demo-notification-tag';
+
+            return self.registration.showNotification(title, {
+                body: body,
+                icon: icon,
+                tag: tag
+            });
+        })
+    );
 });
 
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  // Android doesn’t close the notification when you click on it
-  // See: http://crbug.com/463146
-  event.notification.close();
+    'use strict';
 
-  // This looks to see if the current is already open and
-  // focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url == '/' && 'focus' in client)
-        return client.focus();
-    }
-    if (clients.openWindow)
-      return clients.openWindow('/');
-  }));
+    console.log('On notification click: ', event.notification.tag);
+    // Android doesn’t close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(clients.matchAll({
+        type: "window"
+    }).then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i];
+            if (client.url == '/' && 'focus' in client)
+                return client.focus();
+        }
+        if (clients.openWindow)
+            return clients.openWindow('/');
+    }));
 
 });
